6f63b6b5f8d7e88eb992a8a15aa54160ea826c23
ahmed82git <ahmed.omar.chlef@gmail.com>
2025-12-26 17:48:17 +0100

    navigate

--- Pages/SignupPage/SignupPage.jsx
+++ Pages/SignupPage/SignupPage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./signupPage.css";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 export default function SignupPage() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("");
   console.log(username, email, password, role);
-
+const navigate = useNavigate()
   const handlChange = (e) => {
     const { name, value } = e.target;
     if (name === "username") {
@@ -67,7 +68,7 @@ export default function SignupPage() {
         </div>
         <button type="submit">Signup</button>
         <p>
-          Already have an account? <a href="/login">Login</a>
+          Already have an account? <span onClick={() => navigate('/login')}>Login</span>
         </p>
       </div>
     </div>
