4e55c624af5e62326963dfc03e91b20c859861c8
AbdelMeza <hako.mezaguer13@gmail.com>
2025-12-26 17:45:02 +0100

    feat: update LoginPage with navigation and style improvements

--- Pages/LoginPage/LoginPage.jsx
+++ Pages/LoginPage/LoginPage.jsx
@@ -1,53 +1,55 @@
 import React from "react";
 import { useState } from "react";
 import "./loginPage.css";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const navigate = useNavigate()
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     console.log(email, password);
-  const handlChange = (e) => {
-    const { name, value } = e.target;
-    if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
-  };
+    const handlChange = (e) => {
+        const { name, value } = e.target;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
 
-  return (
-    <div className="login-page">
-      <h1>Login Page</h1>
-      <div className="login-container">
-        <div className="email">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            required
-            onChange={handlChange}
-            value={email}
-          />
-        </div>
-        <br />
-        <div className="passowrd">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            required
-            onChange={handlChange}
-            value={password}
-          />
-        </div>
+    return (
+        <div className="login-page">
+            <h1>Login Page</h1>
+            <div className="login-container">
+                <div className="email">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        required
+                        onChange={handlChange}
+                        value={email}
+                    />
+                </div>
+                <br />
+                <div className="passowrd">
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        required
+                        onChange={handlChange}
+                        value={password}
+                    />
+                </div>
 
-        <button type="submit">Login</button>
-        <p>
-          Don't have an account? <a href="/signup">Sign up</a>
-        </p>
-      </div>
-    </div>
-  );
+                <button type="submit">Login</button>
+                <p>
+                    Don't have an account? <span onClick={() => navigate('/signup')}>Sign up</span>
+                </p>
+            </div>
+        </div>
+    );
 }
