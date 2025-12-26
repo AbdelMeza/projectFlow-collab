1dcbbddccfd8f183faf71548d04331c80b8bf855
AbdelMeza <hako.mezaguer13@gmail.com>
2025-12-26 16:28:24 +0100

    feat: initialize project with Vite, React, and routing setup
    - Added package.json with dependencies for React, React Router, Zustand, and ESLint.
    - Created App component with routes for Signup and Login pages.
    - Added global styles in index.css for consistent theming and layout.
    - Set up main entry point in main.jsx with BrowserRouter for routing.
    - Configured Vite with react plugin in vite.config.js.

---
+++ Pages/SignupPage/SignupPage.jsx
@@ -0,0 +1,5 @@
+export default function SignupPage(){
+    return <div className="signup-page">
+        
+    </div>
+}
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import SignupPage from '../Pages/SignupPage/SignupPage'
+import LoginPage from '../Pages/LoginPage/LoginPage'
+
+function App() {
+  return <Routes>
+    <Route path='/signup' element={<SignupPage/>} ></Route>
+    <Route path='/login' element={<LoginPage/>} ></Route>
+  </Routes>
+}
+
+export default App
